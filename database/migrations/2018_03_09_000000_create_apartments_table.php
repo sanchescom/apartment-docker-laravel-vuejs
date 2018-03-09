46a4ec087ef8c63b45bfd313202ae7e296455ae5
sanchescom <sanches.com@mail.ru>
2018-03-09 14:40:11 +0300

    Migration for apartments

--- database/migrations/2018_03_09_000000_create_apartments_table.php
+++ database/migrations/2018_03_09_000000_create_apartments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateApartmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('apartments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->dateTime('created_at');
+            $table->dateTime('move_in_date');
+            $table->string('street');
+            $table->string('post_code');
+            $table->string('city');
+            $table->string('country');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('token');
         });
     }
 
@@ -30,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('apartments');
     }
 }
